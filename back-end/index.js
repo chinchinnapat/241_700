5cef3bfc39b7c48aec60ddef84411867c84c5950
chinchinnapat <chinnapat.ph@ku.th>
2026-03-06 12:16:36 +0700

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -25,7 +25,10 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 const app = express();
 const port = 8000;
+const cors = require('cors');
 
+
+app.use(cors());
 app.use(bodyParser.json());
 
 
@@ -290,4 +293,5 @@ app.listen(port, async () => {
     await initMysql();
     console.log(`Server is running on http://localhost:${port}`);
 });
+
 
